a31e5caad65501d248b38eb6c157d1f09dbd15b2
DuskyShelf <dupsdale@gmail.com>
2016-05-02 22:09:55 +0100

    renamed buildRender function

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/:route', function (req, res) {
   if (contentFolderContains(req.params.route)) {
     var URL = 'content/' + req.params.route + '/index.md';
     fs.readFile(URL, 'utf-8', function (err, data) {
-      buildRender(res, markdown.toHTML(data));
+      renderFullPage(res, markdown.toHTML(data));
     });
   } else {
     res.status(404).send('404 Page Not Found');
@@ -16,16 +16,16 @@ app.get('/:route', function (req, res) {
 });
 
 // helpers
-var buildRender = function (res, page) {
+var contentFolderContains = function (route) {
+  var folders = fs.readdirSync('content');
+  return folders.indexOf(route) > -1;
+};
+
+var renderFullPage = function (res, page) {
   fs.readFile('template.html', 'utf-8', function (err, data) {
     fullPage = data.replace("{{content}}", page);
     res.send(fullPage);
   });
-};
-
-var contentFolderContains = function (route) {
-  var folders = fs.readdirSync('content');
-  return folders.indexOf(route) > -1;
 };
 
 // run
