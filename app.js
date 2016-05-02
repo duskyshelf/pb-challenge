385499eac10f7975dc3a59a650a29f25125fbca9
DuskyShelf <dupsdale@gmail.com>
2016-05-02 16:14:58 +0100

    passes content tests passing relevant index.md files - does not include template at this point

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
 
+var contentOptions = {
+  root: __dirname + '/content/'
+};
+
 app.get('/about-page', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('/about-page/index.md', contentOptions);
 });
 
 app.get('/jobs', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('/jobs/index.md', contentOptions);
 });
 
 app.get('/valves', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('/valves/index.md', contentOptions);
 });
 
 
